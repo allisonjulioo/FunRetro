72049ed1110c9d1ef654e19362bd6829bbe8218f
Allison Julio <allison.julio@sympla.com.br>
2019-12-23 17:50:07 -0300

    Build to production

---
+++ dist/precache-manifest.5a62856999ff224b1ea85c6554f0893c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f476bbad59f0735d2bf6",
+    "url": "/css/app.b6775406.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "ab6bfe4f0359c689f6ae16f0428bbb3a",
+    "url": "/img/brand_light.ab6bfe4f.svg"
+  },
+  {
+    "revision": "37f6036fd338be940c115f5bdfdaa622",
+    "url": "/img/empty.37f6036f.png"
+  },
+  {
+    "revision": "f8b043d9dd2a286896aef4f83c2f7a94",
+    "url": "/img/pin.f8b043d9.png"
+  },
+  {
+    "revision": "56607d61e2c12102381a9b74575fc680",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f476bbad59f0735d2bf6",
+    "url": "/js/app.3351f0dc.js"
+  },
+  {
+    "revision": "fbbed0c09f9f8901456d",
+    "url": "/js/chunk-vendors.bd73e62f.js"
+  },
+  {
+    "revision": "fb26686dfb9e0768c25f99fa11521a97",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
